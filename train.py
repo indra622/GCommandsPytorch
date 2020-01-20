005a02f4094b20e7f6ffb0b9398a25f5623d420c
Hosung Park <indra622@gmail.com>
2020-01-20 19:33:46 +0900

    code improved for pytorch 1.5

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import torch.nn.functional as F
 from torch.autograd import Variable
+import torch
 
 
 def train(loader, model, optimizer, epoch, cuda, log_interval, verbose=True):
@@ -15,15 +16,15 @@ def train(loader, model, optimizer, epoch, cuda, log_interval, verbose=True):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        global_epoch_loss += loss.data[0]
+        global_epoch_loss += loss.data
         if verbose:
             if batch_idx % log_interval == 0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                     epoch, batch_idx * len(data), len(loader.dataset), 100.
-                    * batch_idx / len(loader), loss.data[0]))
+                    * batch_idx / len(loader), loss.data))
     return global_epoch_loss / len(loader.dataset)
 
-
+@torch.no_grad()
 def test(loader, model, cuda, verbose=True):
     model.eval()
     test_loss = 0
@@ -31,9 +32,9 @@ def test(loader, model, cuda, verbose=True):
     for data, target in loader:
         if cuda:
             data, target = data.cuda(), target.cuda()
-        data, target = Variable(data, volatile=True), Variable(target)
+        data, target = Variable(data), Variable(target)
         output = model(data)
-        test_loss += F.nll_loss(output, target, size_average=False).data[0]  # sum up batch loss
+        test_loss += F.nll_loss(output, target, size_average=False).data  # sum up batch loss
         pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
